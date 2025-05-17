fdd39569d1e82409a29d2f0eed3baefb67e29528
Sifaur Rahman <priontaghosh@gmail.com>
2025-05-17 23:31:35 +0300

    added DB connection with my own mysql info + schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,4 @@
--- I ran this to create tables in my phpMyAdmin --
+-- I ran this to create tables in my phpMyAdmin
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
