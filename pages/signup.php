4e4d44188455659cf5aaace13e753775d4f4412a
Sifaur Rahman <priontaghosh@gmail.com>
2025-05-18 08:33:58 +0300

    added basic signup form layout

---
+++ pages/signup.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+</head>
+<body>
+    <h2>Create an Account</h2>
+    <form method="POST">
+        <label>Username:</label><br>
+        <input type="text" name="username" required><br><br>
+        
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br><br>
+        
+        <input type="submit" value="Sign Up">
+    </form>
+</body>
+</html>
