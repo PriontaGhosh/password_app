519a65e876f15f9d5b5d52317ac319ae3f3dc4cf
Sifaur Rahman <priontaghosh@gmail.com>
2025-05-20 19:56:41 +0300

    added logic to build password from different character groups

--- classes/PasswordGenerator.php
+++ classes/PasswordGenerator.php
@@ -1,9 +1,34 @@
 <?php
-// classes/PasswordGenerator.php
-
 class PasswordGenerator {
     public function generate($length, $upper, $lower, $numbers, $special) {
-        // logic 
-        return "";
+        $upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $lowerChars = 'abcdefghijklmnopqrstuvwxyz';
+        $numberChars = '0123456789';
+        $specialChars = '!@#$%^&*()_+-=[]{}';
+
+        $result = '';
+
+        // randomly pick characters
+        $result .= $this->getRandom($upperChars, $upper);
+        $result .= $this->getRandom($lowerChars, $lower);
+        $result .= $this->getRandom($numberChars, $numbers);
+        $result .= $this->getRandom($specialChars, $special);
+
+        // fill remaining length with random mix
+        $remaining = $length - strlen($result);
+        $allChars = $upperChars . $lowerChars . $numberChars . $specialChars;
+        $result .= $this->getRandom($allChars, $remaining);
+
+        // shuffle final password
+        return str_shuffle($result);
+    }
+
+    private function getRandom($characters, $count) {
+        $output = '';
+        $len = strlen($characters);
+        for ($i = 0; $i < $count; $i++) {
+            $output .= $characters[rand(0, $len - 1)];
+        }
+        return $output;
     }
 }
