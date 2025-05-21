eed6d31895ed6f82b38ac3754adf5b35d38b3a22
Sifaur Rahman <priontaghosh@gmail.com>
2025-05-21 22:45:50 +0300

    created PasswordManager class to encrypt and save passwords

--- classes/PasswordManager.php
+++ classes/PasswordManager.php
@@ -9,4 +9,14 @@ class PasswordManager {
         $this->conn = $conn;
         $this->aesKey = $aesKey;
     }
+
+    public function savePassword($userId, $platform, $plainPassword) {
+        // Encrypt password using AES-128-ECB
+        $encryptedPassword = openssl_encrypt($plainPassword, "AES-128-ECB", $this->aesKey);
+
+        $stmt = $this->conn->prepare("INSERT INTO passwords (user_id, platform, password_data) VALUES (?, ?, ?)");
+        $stmt->bind_param("iss", $userId, $platform, $encryptedPassword);
+
+        return $stmt->execute();
+    }
 }
