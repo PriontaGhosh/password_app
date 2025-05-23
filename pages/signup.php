947ca0ca7c52380971735d6d7927048366c6281c
Sifaur Rahman <priontaghosh@gmail.com>
2025-05-23 15:56:12 +0300

    Added 'Go to Login' button after signup form

--- pages/signup.php
+++ pages/signup.php
@@ -32,15 +32,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <h2>Create an Account</h2>
+
     <form method="POST">
         <label>Username:</label><br>
         <input type="text" name="username" required><br><br>
-        
+
         <label>Password:</label><br>
         <input type="password" name="password" required><br><br>
-        
+
         <input type="submit" value="Sign Up">
     </form>
+
+    <!-- Button to go to login page -->
+    <p>Already have an account?</p>
+    <a href="login.php"><button type="button">Go to Login</button></a>
+
+    <!-- Display messages below -->
+    <p style="color: red;"><?php echo $message ?? ''; ?></p>
 </body>
 </html>
-<p style="color: red;"><?php echo $message ?? ''; ?></p>
